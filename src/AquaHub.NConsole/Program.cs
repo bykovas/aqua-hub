cb6f6b874b295bdbe7905b5e50337c5a2cf6b03c
Denisas Bykovas <bykovas@icloud.com>
2023-11-03 09:18:06 +0200

    Schedules from json

---
+++ src/AquaHub.NConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AquaHub.NConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            while(true)
+            {
+                var values = Schedule.get_current_values();
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()} - Blue plus: {values[0]}, Coral plus: {values[1]}");
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
