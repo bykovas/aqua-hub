e58292fd255facea6191646727eb9ba4b81abccd
Denisas Bykovas <bykovas@icloud.com>
2023-11-21 17:40:04 +0200

    Error handling

--- src/AquaHub.T5LampsController/main.py
+++ src/AquaHub.T5LampsController/main.py
@@ -1,48 +1,67 @@
 import sys
 import time
 import paho.mqtt.client as mqtt
-
+import logging
 from DRF0971driver import *
 
-# Callback function for processing received messages
-def on_message(client, userdata, message):
-    value = int(message.payload.decode())
-    # Set DAC voltage for t5blue channel
-    if message.topic == "ahub/light/t5blue/in":
-        dac.set_dac_out_voltage(value, CHANNEL_0)
-        userdata['t5blue'] = value
-    # Set DAC voltage for t5coral channel
-    elif message.topic == "ahub/light/t5coral/in":
-        dac.set_dac_out_voltage(value, CHANNEL_1)
-        userdata['t5coral'] = value
+# Configure logging with an identifier for the specific application
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - AquaHub.T5LampController - %(levelname)s - %(message)s')
 
+# Function to log messages
+def log_message(message):
+    full_message = f"AquaHub.T5LampController - {message}"
+    try:
+        client.publish("ahub/logs/error", full_message)
+    except Exception:
+        logging.error(full_message)
+
+# Callback function for processing received MQTT messages
+def on_message(client, userdata, message):
+    try:
+        value = int(message.payload.decode())
+        if message.topic == "ahub/light/t5blue/in":
+            dac.set_dac_out_voltage(value, CHANNEL_0)
+            userdata['t5blue'] = value
+        elif message.topic == "ahub/light/t5coral/in":
+            dac.set_dac_out_voltage(value, CHANNEL_1)
+            userdata['t5coral'] = value
+    except ValueError:
+        log_message(f"Error: Invalid value received in topic {message.topic}")
+
+# Main function
 def main() -> int:
-    # Initialize MQTT client
     client_userdata = {'t5blue': 0, 't5coral': 0}
     client = mqtt.Client(userdata=client_userdata)
     client.on_message = on_message
 
-    # Connect to MQTT server
-    client.connect("192.168.1.11")
+    try:
+        client.connect("192.168.1.11")
+    except Exception as e:
+        log_message(f"An error occurred: {e}")
+        sys.exit(1)
 
-    # Subscribe to topics
     client.subscribe("ahub/light/t5blue/in")
     client.subscribe("ahub/light/t5coral/in")
-
-    # Start MQTT loop
     client.loop_start()
 
-    # Initialize DAC with 0 voltage
     dac.set_dac_out_voltage(0, CHANNEL_0)
     dac.set_dac_out_voltage(0, CHANNEL_1)
 
-    while True:
-        # Publish current values to MQTT topics
-        client.publish("ahub/light/t5blue/out", str(client_userdata['t5blue']))
-        client.publish("ahub/light/t5coral/out", str(client_userdata['t5coral']))
+    try:
+        while True:
+            client.publish("ahub/light/t5blue/out", str(client_userdata['t5blue']))
+            client.publish("ahub/light/t5coral/out", str(client_userdata['t5coral']))
+            time.sleep(5)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.loop_stop()
+        client.disconnect()
 
-        time.sleep(5)
-
+# Entry point
 if __name__ == '__main__':
-    dac = DRF0971Driver()
-    sys.exit(main())
+    try:
+        dac = DRF0971Driver()
+        sys.exit(main())
+    except Exception as e:
+        log_message(f"An error occurred: {e}")
