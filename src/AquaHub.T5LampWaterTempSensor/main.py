7fa67a694f5dca1e805e3e8b9ec98adbafd56230
Denisas Bykovas <bykovas@icloud.com>
2023-11-24 21:57:38 +0200

    add debug logging

--- src/AquaHub.T5LampWaterTempSensor/main.py
+++ src/AquaHub.T5LampWaterTempSensor/main.py
@@ -6,26 +6,50 @@ import os
 from w1thermsensor import W1ThermSensor
 import paho.mqtt.client as mqtt
 
+# MQTT Event Handlers
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        logging.info("Connected to MQTT Broker!")
+    else:
+        logging.error(f"Failed to connect, return code {rc}")
+
+def on_disconnect(client, userdata, rc):
+    logging.warning("Disconnected from MQTT Broker")
+
+def on_publish(client, userdata, mid):
+    logging.info(f"Message {mid} published")
+
 # Load configuration from a JSON file
 with open('/apps/aquahub.t5lamp.appsettings.json', 'r') as config_file:
     config = json.load(config_file)
 
 # Setup logging
-log_file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ds18b20_sensor.log')
+log_file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logs/ds18b20_sensor.log')
 logging.basicConfig(handlers=[TimedRotatingFileHandler(log_file_name, when="midnight", interval=1, backupCount=7)],
-                    level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+                    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Initialize MQTT Client
 client = mqtt.Client("DS18B20_Publisher")
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_publish = on_publish
+
+# Connect to MQTT Broker
 client.connect(config['MQTT_SERVER'])
 
+# Initialize sensor
 sensor = W1ThermSensor()
 
 while True:
     try:
+        logging.info("Starting loop cycle")                
         temperature = round(sensor.get_temperature(), 2)
+        logging.info(f"Measured temperature: {temperature}")        
         client.publish(config['DS18B20_TOPIC'], temperature)
+        logging.info(f"Published temperature: {temperature} to MQTT topic {config['DS18B20_TOPIC']}")
     except Exception as e:
-        logging.error(f"Error reading from DS18B20: {e}")
+        logging.error(f"Error reading from DS18B20 or publishing to MQTT: {e}")
 
-    update_interval = config.get('T5LAMP_UPDATE_INTERVAL', 5) # Default to 5 seconds if not set
+    # Sleep for the configured update interval
+    update_interval = config.get('T5LAMP_UPDATE_INTERVAL', 5)  # Default to 5 seconds if not set
     time.sleep(update_interval)
